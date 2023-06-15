4b9a86be4505b271189a740cd61b310384dde054
Alex Washburn <awashburn@Alexs-MBP.hsd1.ca.comcast.net>
2023-06-15 11:58:55 -0700

    changes to main.py initial file. Plotting actigraphy data now working.

--- main.py
+++ main.py
@@ -4,17 +4,117 @@ from pyActigraphy.analysis import SSA
 import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
+import plotly.offline as pyo    # debug import
 import os
 
-# Get the directory path of the current script or module
-fpath = '/Users/awashburn/Library/CloudStorage/OneDrive-BowdoinCollege/Documents/Mormino-Lab-Internship/Python-Projects/Actigraphy-Testing/timeSeries-actigraphy-csv-files/'
+def read_input_data(filename):
+    """
+    Reads in a single cwa file for actigraphy analysis
+    :param filename: the name of the file to read in
+    :return: raw processed file
+    """
 
-# actually read in the data
-raw = pyActigraphy.io.read_raw_bba(fpath+'79036_0000000504-timeSeries.csv.gz')
+    # Get the directory path of the current script or module
+    fpath = '/Users/awashburn/Library/CloudStorage/OneDrive-BowdoinCollege/Documents/Mormino-Lab-Internship/Python-Projects/Actigraphy-Testing/timeSeries-actigraphy-csv-files/'
 
-# verify that this was done correctly
-print(raw.name)
-print(raw.start_time)
+    # actually read in the data
+    raw = pyActigraphy.io.read_raw_bba(fpath+filename)
+
+    # Retrieve header information from the data set
+    file_name = raw.name
+    print('file name: ' + file_name)
+
+    file_start_time = raw.start_time
+    print('file start time: ')
+    print(file_start_time)
+
+    file_data_acquisition_duration = raw.duration()
+    print('data acquisition duration: ')
+    print(file_data_acquisition_duration)
+
+    device_serial_number = raw.uuid
+    print('device serial number: ')
+    print(device_serial_number)
+
+    acquisition_frequency = raw.frequency
+    print('acquisition frequency: ')
+    print(acquisition_frequency)
+
+    return raw
+
+
+# create a layout for all future plotting
+layout = go.Layout(
+    title = "Actigraphy data",
+    xaxis = dict(title="Date time"),
+    yaxis = dict(title="Counts/period"),
+    showlegend = False
+)
+
+'''PLOTTING THE DATA - MULTI-DAY'''
+def multi_day_plot(raw_data_file, layout):
+    """
+    generates a plot of multi-day actigraphy data
+    :param raw_data_file: the raw data file for the actigraphy plot
+    :param layout: a pre-defined layout for the graph
+    :return: no return value
+    """
+
+    # create the figure
+    fig = go.Figure(data=[go.Scatter(x=raw.data.index.astype(str), y=raw.data)], layout=layout)
+
+    # display the figure
+    pyo.plot(fig)
+
+
+'''DAILY ACTIVITY PROFILE'''
+def daily_activity_profile(raw_data_file, layout):
+    """
+    generates a daily activity profile (subset of multi day actigraphy plot)
+    :param raw_data_file: the raw data file for the actigraphy plot
+    :param layout: a pre-defined layout for the graph
+    :return:
+    """
+    layout.update(title="Daily activity profile",xaxis=dict(title="Date time"), showlegend=False);
+    help(raw.average_daily_activity)
+
+    daily_profile = raw.average_daily_activity(freq='15min', cyclic=False, binarize=False) # frequency = data resampling frequency
+
+    # create the daily activity profile figure
+    daily_fig = go.Figure(data=[
+        go.Scatter(x=daily_profile.index.astype(str), y=daily_profile)
+    ], layout=layout)
+
+    # plot the daily figure
+    pyo.plot(daily_fig)
+
+
+'''ACTIVITY ONSET AND OFFSET TIMES'''
+def retrieve_activity_onset_offset(raw_data_file):
+    """
+    This function will retrieve the time of activity onset and offset.
+    :param raw_data_file: the raw data file containing actigraphy data
+    :return: a list containing the activity onset time followed by the activity offset time
+    """
+    activity_onset = raw.AonT(freq='15min', binarize=True)
+    activity_offset = raw.AoffT(freq='15min', binarize=True)
+
+    print('activity onset: ')
+    print(activity_onset)
+
+    print('activity offset: ')
+    print(activity_offset)
+
+    return [activity_onset, activity_offset]
+
+
+'''FUNCTION CALLS'''
+
+raw = read_input_data('79036_0000000504-timeSeries.csv.gz')
+multi_day_plot(raw, layout)
+#daily_activity_profile(raw, layout)
+#retrieve_activity_onset_offset(raw)
+
 
 
 
