9ef5930a985da8e2c6c0142fb27cd207a21011fd
alexwashburn1 <awashburn@bowdoin.edu>
2023-06-28 08:42:07 -0700

    sleep duration calculations

--- compute_sleep_period_and_duration.py
+++ compute_sleep_period_and_duration.py
@@ -8,6 +8,10 @@ import plotly.offline as pyo    # debug import
 import os
 import re
 import pyexcel_ods3
+from datetime import timedelta
+from dateutil import parser
+
+
 
 def read_input_data(filename):
     """
@@ -35,8 +39,18 @@ def ck_0_1_classification_single_file(raw, output_path, file_ID):
     # write the binarized csv to the correct output path
     ck.to_csv(output_path + 'ck_0_1_scoring_scoring_' + file_ID + '.csv')
 
+def sadeh_0_1_classification_single_file(raw, output_path, file_ID):
+    """
+    Generates the binarized classification for sleep and wake (Cole-Kripke) for a particular raw file.
+    :param raw: the raw file
+    """
 
-def ck_0_1_classification_all_files(raw_input_path, sleep_diary_path, output_path):
+    sadeh = raw.Sadeh()
+    # write the binarized csv to the correct output path
+    sadeh.to_csv(output_path + 'sadeh_0_1_scoring_scoring_' + file_ID + '.csv')
+
+
+def sleep_0_1_classification_all_files(raw_input_path, sleep_diary_path, output_path, sadeh=True, ck=False):
    """
    Determines all of the raw files to classify by looking for which ones have a valid sleep log.
     After determining which ones have a valid sleep log, creates a raw object for them, and based on the raw object
@@ -44,6 +58,8 @@ def ck_0_1_classification_all_files(raw_input_path, sleep_diary_path, output_pat
    :param raw_input_path: the input path of the raw files (.csv.gz)
    :param sleep_diary_path: the directory of the sleep diaries
    :param output_path: the path to write the binarized ck objects to
+   :param sadeh: if true, 0 1 classification for sadeh will be generated. if false, then not.
+   :param ck: if true, 0 1 classification for ck will be generated. if false, then not.
    """
 
    # determine which IDs have a valid sleep journal
@@ -61,9 +77,21 @@ def ck_0_1_classification_all_files(raw_input_path, sleep_diary_path, output_pat
    for file_ID in file_names_without_extension:
        full_file_name = file_ID + '-timeSeries.csv.gz'
        raw = read_input_data(full_file_name)
-       # create 0 1 binarized sleep scoring for the file, and write it to the output csv path
-       ck_0_1_classification_single_file(raw, output_path, file_ID)
 
+       # create 0 1 binarized sleep scoring for the file, and write it to the output csv path
+       # look at sadeh and ck parameters to see which one will be created.
+       if ck == True and sadeh == False:
+           ck_0_1_classification_single_file(raw, output_path, file_ID)
+       else:
+           if sadeh == True and ck == False:
+               sadeh_0_1_classification_single_file(raw, output_path, file_ID)
+
+
+
+from datetime import timedelta
+import re
+import pandas as pd
+import pyexcel_ods3
 
 def calculate_sleep_period(ck_0_1_file, sleep_journal_file, ck_file_path, sleep_journal_file_path):
     """
@@ -73,79 +101,60 @@ def calculate_sleep_period(ck_0_1_file, sleep_journal_file, ck_file_path, sleep_
     """
 
     # read in the ck_0_1 file, and convert it to a dataframe
-    ck_df = pd.read_csv(ck_file_path+ck_0_1_file)
+    ck_df = pd.read_csv(ck_file_path + ck_0_1_file)
 
     # Remove the trailing part from the "time" column using regular expressions
     ck_df['time'] = ck_df['time'].apply(lambda x: re.sub(r'\:\d{2}-\d{2}:\d{2}$', '', x))
     print('ck df: ', ck_df)
 
     # read in the sleep journal file, and convert it to a data frame
-    data = pyexcel_ods3.get_data(sleep_journal_file_path+sleep_journal_file)
+    data = pyexcel_ods3.get_data(sleep_journal_file_path + sleep_journal_file)
     sleep_journal_df = pd.DataFrame(data[next(iter(data))])
     print('sleep_journal_df: ', sleep_journal_df)
 
-    # DETERMINE SLEEP PERIOD
+    # Create a new DataFrame to store the sleep period details
+    result_df = pd.DataFrame(columns=['night', 'computed duration', 'journal logged sleep duration'])
 
-    # Iterate over each row in 'ck_df'
-    for ck_row in ck_df.itertuples(index=True):
-        ck_time = ck_row.time
+    # Iterate over each row in 'sleep_journal_df' starting from the 4th row
+    for i, journal_row in enumerate(sleep_journal_df.iloc[3:].itertuples(index=False), start=1):
+        journal_start = journal_row[1]  # Access the second column (index 1)
+        journal_end = journal_row[2]  # Access the third column (index 2)
 
-        # Flag variable to control the iteration over 'sleep_journal_df'
-        match_found = False
+        # Find the row in 'ck_df' where 'time' matches the 'START' value of the sleep journal
+        start_row = ck_df[ck_df['time'] == journal_start].iloc[0]
 
-        # Iterate over each row in 'sleep_journal_df' only if a match is not found
-        for journal_row in sleep_journal_df.iloc[3:].itertuples(index=False):
-            journal_start = journal_row[1]  # Access the second column (index 1)
+        # Find the row in 'ck_df' where 'time' matches the 'END' value of the sleep journal
+        end_row = ck_df[ck_df['time'] == journal_end].iloc[0]
 
-            # Check if the value for 'time' column in 'ck_df' matches the 'START' value in 'sleep_journal_df'
-            if ck_time == journal_start:
-                match_found = True
-                print(True)
-                print(ck_time)
-                print(journal_start)
+        # Calculate the duration by counting the number of rows with 'acc' value equal to 1
+        duration = ck_df.loc[start_row.name:end_row.name, 'acc'].eq(1).sum() - 1
 
-                # Get the index of the matched row in 'ck_df'
-                ck_row_index = ck_row.Index
+        # Convert duration to timedelta format
+        duration_timedelta = timedelta(minutes=int(duration))
 
-                # Check if there are at least 15 rows before the matched row
-                if ck_row_index >= 15:
-                    # Flag variable to track three consecutive rows with 'acc' value equal to 1
-                    consecutive_found = False
+        # Format the duration as hh:mm
+        duration_formatted = str(duration_timedelta)
+
+        # Calculate the journal logged sleep duration
+        journal_duration = str(parser.parse(journal_end) - parser.parse(journal_start))
+
+        # Add the sleep period details to the result DataFrame
+        result_df = result_df.append({'night': i, 'computed duration': duration_formatted, 'journal logged sleep duration': journal_duration}, ignore_index=True)
+
+    # Get the ID from the sleep_journal_file
+    id = sleep_journal_file.split('.')[0]
+
+    # Construct the output file path
+    sadeh_or_ck = 'ck' # NOTE - can change to whichever is being generated. TODO - add this as a parameter.
+    output_file_path = os.path.join(ck_file_path, f"{id}_sleep_duration_metrics_" + sadeh_or_ck + ".csv")
+
+    # Write the result DataFrame to CSV
+    result_df.to_csv(output_file_path, index=False)
+
+    # Print the result DataFrame
+    print(result_df)
 
 
-                    # Iterate forward from 15 rows before the matched row
-                    for i in range(ck_row_index - 15, ck_row_index + 1):
-                        if ck_df.loc[i, 'acc'] == 1:
-                            if not consecutive_found:
-                                # Start of a potential three consecutive rows with 'acc' value equal to 1
-                                consecutive_found = True
-                                start_index = i
-                        elif consecutive_found:
-                            # Check if three consecutive rows have 'acc' value equal to 1
-                            if i - start_index >= 2:
-                                start_time = ck_df.loc[start_index, 'time']
-                                #print("Start Time:", start_time)
-
-                                # Print the first row of the three consecutive rows
-                                print('first row of the three consecutive rows: ', ck_df.iloc[start_index])
-
-                                break
-                            else:
-                                # Reset flag if consecutive rows are not enough
-                                consecutive_found = False
-
-                    # Check if three consecutive rows were not found
-                    if not consecutive_found:
-                        print("No three consecutive rows found with 'acc' value equal to 1.")
-
-                else:
-                    print("Not enough rows before the matched row.")
-
-                break
-
-        # If a match is not found, print False and proceed to the next row in 'ck_df'
-        if not match_found:
-            continue
 
 
 '''FUNCTION CALLS'''
@@ -160,9 +169,87 @@ output_path = '/Users/awashburn/Library/CloudStorage/OneDrive-BowdoinCollege/Doc
                  'Mormino-Lab-Internship/Python-Projects/Actigraphy-Testing/timeSeries-actigraphy-csv-files/ck_sleep_0_1_scoring/'
 
 
-#ck_0_1_classification_all_files(raw_input_path, sleep_diary_path, output_path)
+#sleep_0_1_classification_all_files(raw_input_path, sleep_diary_path, output_path)
 
-calculate_sleep_period('ck_0_1_scoring_scoring_78203_0000000613.csv', '78203_0000000613.ods', output_path, sleep_diary_path)
+file_id = '79036_0000000504'
+
+sadeh_or_ck = 'ck'
+
+calculate_sleep_period(sadeh_or_ck + '_0_1_scoring_scoring_' + file_id + '.csv', file_id + '.ods', output_path, sleep_diary_path)
+
+
+
+
+
+
+
+
+
+
+
+# OLD:
+# # DETERMINE SLEEP PERIOD
+    #
+    # # Iterate over each row in 'ck_df'
+    # for ck_row in ck_df.itertuples(index=True):
+    #     ck_time = ck_row.time
+    #
+    #     # Flag variable to control the iteration over 'sleep_journal_df'
+    #     match_found = False
+    #
+    #     # Iterate over each row in 'sleep_journal_df' only if a match is not found
+    #     for journal_row in sleep_journal_df.iloc[3:].itertuples(index=False):
+    #         journal_start = journal_row[1]  # Access the second column (index 1)
+    #
+    #         # Check if the value for 'time' column in 'ck_df' matches the 'START' value in 'sleep_journal_df'
+    #         if ck_time == journal_start:
+    #             match_found = True
+    #             print(True)
+    #             print(ck_time)
+    #             print(journal_start)
+    #
+    #             # Get the index of the matched row in 'ck_df'
+    #             ck_row_index = ck_row.Index
+    #
+    #             # Check if there are at least 15 rows before the matched row
+    #             if ck_row_index >= 15:
+    #                 # Flag variable to track three consecutive rows with 'acc' value equal to 1
+    #                 consecutive_found = False
+    #
+    #
+    #                 # Iterate forward from 15 rows before the matched row
+    #                 for i in range(ck_row_index - 15, ck_row_index + 1):
+    #                     if ck_df.loc[i, 'acc'] == 1:
+    #                         if not consecutive_found:
+    #                             # Start of a potential three consecutive rows with 'acc' value equal to 1
+    #                             consecutive_found = True
+    #                             start_index = i
+    #                     elif consecutive_found:
+    #                         # Check if three consecutive rows have 'acc' value equal to 1
+    #                         if i - start_index >= 2:
+    #                             start_time = ck_df.loc[start_index, 'time']
+    #                             #print("Start Time:", start_time)
+    #
+    #                             # Print the first row of the three consecutive rows
+    #                             print('first row of the three consecutive rows: ', ck_df.iloc[start_index])
+    #
+    #                             break
+    #                         else:
+    #                             # Reset flag if consecutive rows are not enough
+    #                             consecutive_found = False
+    #
+    #                 # Check if three consecutive rows were not found
+    #                 if not consecutive_found:
+    #                     print("No three consecutive rows found with 'acc' value equal to 1.")
+    #
+    #             else:
+    #                 print("Not enough rows before the matched row.")
+    #
+    #             break
+    #
+    #     # If a match is not found, print False and proceed to the next row in 'ck_df'
+    #     if not match_found:
+    #         continue
 
 
 
--- convert_sleep_journal_proper_format.py
+++ convert_sleep_journal_proper_format.py
@@ -5,6 +5,11 @@ import os
 import csv
 # import pandas as pd
 from datetime import datetime
+from datetime import timedelta
+import pyexcel_ods3
+from dateutil import parser
+from datetime import timedelta
+from datetime import time
 
 
 
@@ -13,17 +18,52 @@ import csv
 import pyexcel_ods3
 from dateutil import parser
 
+# def create_sleep_diary_ods(original_csv_file):
+#     df = pd.read_csv(original_csv_file, delimiter=',')  # Update delimiter if necessary
+#
+#     for _, row in df.iterrows():
+#         subject_id = row['ID']
+#         new_ods_file = f"/Users/awashburn/Library/CloudStorage/OneDrive-BowdoinCollege/Documents/Mormino-Lab-Internship/Python-Projects/Actigraphy-Testing/timeSeries-actigraphy-csv-files/sleep-journals/{subject_id}.ods"
+#
+#         sheet_data = [["SubjectID", subject_id]]  # Add extra row with SubjectID
+#         sheet_data.append([])  # Add blank row
+#
+#         sheet_data.append(["TYPE", "START", "END"])  # Write the header row
+#
+#         for i in range(1, 14):
+#             date = row[f'D{i}_date']
+#             wakeup = row[f'D{i+1}_wakeup']
+#             onset = row[f'D{i}_onset']
+#
+#             if pd.notnull(date) and pd.notnull(wakeup) and pd.notnull(onset) and date != '########' and wakeup != '########' and onset != '########':
+#                 date_format = '%d/%m/%y' if len(date.split('/')[2]) == 2 else '%d/%m/%Y'
+#
+#                 if len(date.split('/')[2]) == 2:
+#                     date = date[:-2] + '20' + date[-2:]  # Assuming all years are in the 2000s
+#
+#                 onset_time = parser.parse(onset).strftime('%H:%M')
+#                 start = parser.parse(date, dayfirst=True).strftime('%Y-%m-%d') + ' ' + onset_time
+#
+#                 wakeup_time = parser.parse(wakeup).strftime('%H:%M')
+#                 end = parser.parse(row[f'D{i+1}_date'], dayfirst=True).strftime('%Y-%m-%d') + ' ' + wakeup_time
+#
+#                 sheet_data.append(['NIGHT', start, end])
+#
+#         # Save as .ods file
+#         pyexcel_ods3.save_data(new_ods_file, {"Sheet 1": sheet_data})
+
+
+
 def create_sleep_diary_ods(original_csv_file):
-    df = pd.read_csv(original_csv_file, delimiter=',')  # Update delimiter if necessary
+    df = pd.read_csv(original_csv_file, delimiter=',')
 
     for _, row in df.iterrows():
         subject_id = row['ID']
         new_ods_file = f"/Users/awashburn/Library/CloudStorage/OneDrive-BowdoinCollege/Documents/Mormino-Lab-Internship/Python-Projects/Actigraphy-Testing/timeSeries-actigraphy-csv-files/sleep-journals/{subject_id}.ods"
 
-        sheet_data = [["SubjectID", subject_id]]  # Add extra row with SubjectID
-        sheet_data.append([])  # Add blank row
-
-        sheet_data.append(["TYPE", "START", "END"])  # Write the header row
+        sheet_data = [["SubjectID", subject_id]]
+        sheet_data.append([])
+        sheet_data.append(["TYPE", "START", "END"])
 
         for i in range(1, 14):
             date = row[f'D{i}_date']
@@ -34,10 +74,16 @@ def create_sleep_diary_ods(original_csv_file):
                 date_format = '%d/%m/%y' if len(date.split('/')[2]) == 2 else '%d/%m/%Y'
 
                 if len(date.split('/')[2]) == 2:
-                    date = date[:-2] + '20' + date[-2:]  # Assuming all years are in the 2000s
+                    date = date[:-2] + '20' + date[-2:]
 
-                onset_time = parser.parse(onset).strftime('%H:%M')
-                start = parser.parse(date, dayfirst=True).strftime('%Y-%m-%d') + ' ' + onset_time
+                onset_time = parser.parse(onset).time()
+                start = parser.parse(date, dayfirst=True)
+
+                if time(0, 0) <= onset_time < time(8, 0):
+                    # Advance the start date by one day if the onset time is at or beyond 00:00:00 am but before 8:00:00 am
+                    start += timedelta(days=1)
+
+                start = start.strftime('%Y-%m-%d') + ' ' + onset_time.strftime('%H:%M')
 
                 wakeup_time = parser.parse(wakeup).strftime('%H:%M')
                 end = parser.parse(row[f'D{i+1}_date'], dayfirst=True).strftime('%Y-%m-%d') + ' ' + wakeup_time
@@ -51,21 +97,3 @@ def create_sleep_diary_ods(original_csv_file):
 original_csv_file = "/Users/awashburn/Library/CloudStorage/OneDrive-BowdoinCollege/Documents/Mormino-Lab-Internship/Python-Projects/Actigraphy-Testing/timeSeries-actigraphy-csv-files/sleep-journals/original_sleep_logs.csv"
 
 create_sleep_diary_ods(original_csv_file)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
